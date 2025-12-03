9d693ec4617f7246657c8332750c510cfac22de1
Yevheniy Yevtushenko <Genya42111@gmail.com>
2025-12-03 14:04:14 +0200

    chore(deps): Add django_htmx libriry

--- src/notebook/notebook/settings.py
+++ src/notebook/notebook/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "django_filters",
     "widget_tweaks",
     "crispy_forms",
+    "crispy_bootstrap5",
     "django_extensions",
+    "django_htmx",
 
     "tasks.apps.TasksConfig",
 ]
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "notebook.urls"
@@ -129,3 +133,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
